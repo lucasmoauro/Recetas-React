cb15ecdd140d3bb488b913ea560dc180ded14d55
Lucas <lucasmoauro.93@gmail.com>
2022-01-06 21:50:03 -0300

    Agregado de tarjetas con mock data

--- src/home/HomeScreen.js
+++ src/home/HomeScreen.js
@@ -1,7 +1,17 @@
+import data from "../lib/mock/recipesMock.json";
+import "../styles/recipeCards.scss";
+
 export const HomeScreen = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	return (
+		<div className="card-container">
+			{data.map((recipe, i) => {
+				return (
+					<div className="card" key={i}>
+						<h1>{recipe.title}</h1>
+						<p>{recipe.description}</p>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
